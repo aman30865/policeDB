a5719deaf442beda38bfdefc97b7a4e63438f539
Aman Prakash <aman30865@gmail.com>
2019-10-28 18:30:40 +0530

    cookies problem solved and bootstrap added

--- index.php
+++ index.php
@@ -1,23 +1,19 @@
 <?php
     session_start();
-    require_once "config.php";
-    
+    require_once "config.php";   
     $error = ""; 
-
 /*----------------------LOGOUT-----------------------------------------*/
-
     if (array_key_exists("logout", $_GET)) {
             unset($_SESSION);
             setcookie("id", "", time() - 3600);
-            $_COOKIE["id"] = "";      
+            $_COOKIE["id"] = "";  
+            session_destroy();
         } 
     else if ((array_key_exists("id", $_SESSION) AND $_SESSION['id']) AND
              (array_key_exists("id", $_COOKIE) AND $_COOKIE['id'])) {
               header("Location: loggedinpage.php");
         }
-
 /*-----------------------SIGNUP----------------------------------------*/
-
     if (array_key_exists("submit", $_POST)) {
         if (!$_POST['AADHAR']) {
             $error .= "A AADHAR is required<br>";
@@ -83,26 +79,71 @@
                 }
         }
     }
-
 ?>
 
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags always come first -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
+    <style>
+      html { 
+          background: url(back.jpg) no-repeat center center fixed; 
+          -webkit-background-size: cover;
+          -moz-background-size: cover;
+          -o-background-size: cover;
+          background-size: cover;
+          }
+      body {  
+              background: none;
+              color: #FFF; 
+          }
+    </style>
+    </head>
+<body>     
 <div id="error"><?php echo $error; ?></div>
+<div id="container">
+    <form method="post" id = "signUpForm">
+        <fieldset class="form-group">
+        <input type="text" name="name" placeholder="Your Name" required>
+        </fieldset>
+        <fieldset class="form-group">
+        <input type="text" name="AADHAR" placeholder="Your AADHAR" required>
+        </fieldset>
+        <fieldset class="form-group">
+        <input type="password" name="password" placeholder="Password" required>
+        </fieldset>
+        <fieldset class="form-group">
+        <input type="checkbox" name="stayLoggedIn" value=1>
+        <input type="submit" name="submit" value="Sign Up!">
+        </fieldset>
+    </form>
 
-<form method="post">
-    <input type="text" name="name" placeholder="Your Name" required>
-    <input type="text" name="AADHAR" placeholder="Your AADHAR" required>
-    <input type="password" name="password" placeholder="Password" required>
-    <input type="checkbox" name="stayLoggedIn" value=1>
-    <input type="hidden" name="signUp" value="1">
-    <input type="submit" name="submit" value="Sign Up!">
-</form>
-
-<form method="post">
-    <input type="text" name="AADHAR" placeholder="AADHAR Number" required>
-    <input type="password" name="password" placeholder="Password" required>
-    <input type="checkbox" name="stayLoggedIn" value=1>
-    <input type="hidden" name="signUp" value="0">
-    <input type="submit" name="submit" value="Log In!">
-</form>
-
+    <form method="post">
+        <input type="text" name="AADHAR" placeholder="AADHAR Number" required>
+        <input type="password" name="password" placeholder="Password" required>
+        <input type="checkbox" name="stayLoggedIn" value=1>
+        <input type="hidden" name="signUp" value="0">
+        <input type="submit" name="submit" value="Log In!">
+    </form>
+</div>
+    
+    
+    
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
+      
+      <script type="text/javascript">
+      
+      </script>
+  </body>
+</html>
 
